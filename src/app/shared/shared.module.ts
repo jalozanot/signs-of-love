cc04763671fd2f32fd40c086da8f5bf37f89e307
jesus lozano <jalozanot@gmail.com>
2020-05-22 22:03:55 -0500

    agregando el footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, PipesComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [],
+  exports: [NavbarComponent],
 })
 export class SharedModule { }
